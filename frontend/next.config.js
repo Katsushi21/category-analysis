0005ad9f233bc984ecd2c6a8d746b73665b2abe0
Katsushi21 <takano@signity.jp>
2025-05-21 19:06:01 +0900

    ✨assetPrefixの追加

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
   env: {
     NEXT_PUBLIC_API_BASE_URL:
       process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8001/api",
